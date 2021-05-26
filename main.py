834f03247ed7a52bd43669aba155c256f2cea814
RIsHaV <rishabvh1024@gmail.com>
2021-05-26 09:02:53 +0530

    Now Education can also be extracted || Updated skills.csv

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import re
 
 # spaCy is a free open-source library for Natural Language Processing in Python.
 # Command to install library : pip install spacy
-# Also we need the Multi-language spaCy module to read names
-# To download this module type in cmd : spacy download xx_ent_wiki_sm
+# Also we need the Multi-language spaCy module to read names and the English module
+# To download this module type in cmd : spacy download xx_ent_wiki_sm/en_core_web_sm
 import spacy
 
 
@@ -31,9 +31,12 @@ from pdfminer.high_level import extract_text
 import pandas as pd
 
 
+
 # import and generate all stopwords to be removed
 from nltk.corpus import stopwords
 STOPWORDS = set(stopwords.words('english'))
+
+
 
 
 
@@ -46,12 +49,13 @@ EMAIL_PATTERN = re.compile(r'[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+')
 
 PHONE_NUMBER_PATTERN = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
 
+
 # Common Education Degrees
 EDUCATION = [
             'BE','B.E.', 'B.E', 'BS', 'B.S',
             'ME', 'M.E', 'M.E.', 'MS', 'M.S',
-            'BTECH', 'B.TECH', 'M.TECH', 'MTECH',
-            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+            'BTECH', 'B.TECH', 'M.TECH', 'MTECH', 'MASTERS',
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII', 'ISC'
         ]
 
 
@@ -123,29 +127,44 @@ def extract_skills(resume_text):
 
 
 
+# Function to extract the Education degree of the applicant
+def extract_education(resume_text):
+    # load pre-trained model
+    nlp = spacy.load('en_core_web_sm')
+
+    nlp_text = nlp(resume_text)
+
+
+    # Sentence Tokenizer
+    nlp_text = [sent.text.strip() for sent in nlp_text]
+
+    edu = {}
+    # Extract education degree
+    for index, text in enumerate(nlp_text):
+        for tex in text.split():
+            # Replace all special symbols
+            tex = re.sub(r'[?|$|.|!|,]', r'', tex)
+            if tex.upper() in EDUCATION and tex not in STOPWORDS:
+                edu[tex] = text + nlp_text[index + 1]
+
+    # Extract year
+    education = []
+    for key in edu.keys():
+        year = re.search(re.compile(r'(((20|19)(\d{2})))'), edu[key])
+        if year:
+            education.append((key, ''.join(year[0])))
+        else:
+            education.append(key)
+    return education
+
+
 
 # driver function
 if __name__ == '__main__':
 
+    f = open('info.txt', 'w')
+
     resume_text = extract_text_from_pdf('./resume.pdf')
-    resume_text = resume_text.replace(',',' ')
-
-    list_of_emails = extract_emails(resume_text)
-
-
-    # The first email that appears above others is generally the applicant’s actual email address,
-    # since people tend to place their contact details in the header section of their resumes.
-    if len(list_of_emails) > 0 :
-        print('Applicants Email Address : ' + list_of_emails[0])
-
-
-
-    list_of_phone_numbers = extract_phone_numbers(resume_text)
-
-    if len(list_of_phone_numbers) > 0 :
-        print('Applicants Phone Number : ' + list_of_phone_numbers[0])
-
-
 
 
     person_names = extract_names(resume_text)
@@ -153,19 +172,56 @@ if __name__ == '__main__':
 
     # The first valid name that appears above others is generally the applicant’s actual name,
     # since people tend to place their contact details in the header section of their resumes.
-    for i in range(0,len(person_names)) :
-        if person_names[i] == '\n' :
+    for i in range(0, len(person_names)):
+        if person_names[i] == '\n':
             pos = i
             break
 
-
     # Extracting the first name
-    name = person_names[:pos+1]
-    print('Applicants Name : ' + name)
+    name = person_names[:pos + 1]
+    f.write('\nName of Applicant : ' + name + '\n\n')
+
+
+
+
+    list_of_emails = extract_emails(resume_text)
+
+    # The first email that appears above others is generally the applicant’s actual email address,
+    # since people tend to place their contact details in the header section of their resumes.
+    f.write('Applicants Email Address : ')
+    if len(list_of_emails) > 0 :
+        f.write(list_of_emails[0])
+    f.write('\n\n')
+
+
+
+
+    list_of_phone_numbers = extract_phone_numbers(resume_text)
+
+    f.write('Applicants Phone Number : ')
+    if len(list_of_phone_numbers) > 0 :
+        if len(list_of_phone_numbers[0]) > 9 and len(list_of_phone_numbers[0]) <13 :
+            f.write(list_of_phone_numbers[0])
+    f.write('\n\n')
+
+
+
 
 
     # Extracting skills
-    print('Applicants Skills : \n')
+    f.write('Applicants Skills : \n\n')
     skills = extract_skills(resume_text)
-    print(skills)
+    for data in skills :
+        f.write(data + '\n')
+    f.write('\n')
+
+
+
+
+    # Extracting Education
+    f.write('Applicants Education : \n\n')
+    education = extract_education(resume_text)
+    for data in education :
+        f.write(data + '\n')
+    f.write('\n')
 
